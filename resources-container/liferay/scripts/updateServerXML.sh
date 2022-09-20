23c9c5c2cc8631969fcb6e9ab7ef8e18eaef9cde
Thierry Touin <thierrytouin@gmail.com>
2022-09-20 10:36:34 +0200

    test ssl

--- resources-container/liferay/scripts/updateServerXML.sh
+++ resources-container/liferay/scripts/updateServerXML.sh
@@ -1,19 +1,26 @@
+## Delete old keystore
+## Delete old keystore
+rm -f /opt/liferay/ssl/cacerts.jks
+
+
+keytool -import -trustcacerts -keystore /opt/liferay/ssl/cacerts.jks \
+   -storepass password -noprompt -alias server-alias -file /opt/liferay/ssl/certificate.pem
+
 cp /opt/liferay/tomcat/conf/server.xml /opt/liferay/tomcat/conf/server.xml_bk
 
-sed -i '101d;112d' /opt/liferay/tomcat/conf/server.xml
+sed -i '86d;94d' /opt/liferay/tomcat/conf/server.xml
 
-STR1='certificateKeyFile=\"conf\/localhost-rsa-key.pem\"'
-STR2='certificateKeyFile=\"\/opt\/liferay\/ssl\/private.key\"'
+STR1='certificateKeystoreFile\=\"conf\/localhost-rsa.jks\"'
+STR2='certificateKeystoreFile=\"\/opt\/liferay\/ssl\/cacerts.jks\" certificateKeystorePassword=\"password\" certificateKeyAlias=\"server-alias\" certificateKeystoreType=\"PKCS12\"'
 sed -i 's/'"$STR1"'/'"$STR2"'/g' /opt/liferay/tomcat/conf/server.xml
 
-STR3='certificateFile=\"conf\/localhost-rsa-cert.pem\"'
-STR4='certificateFile=\"\/opt\/liferay\/ssl\/certificate.pem\" certificateKeyPassword=\"password\"'
-sed -i 's/'"$STR3"'/'"$STR4"'/g' /opt/liferay/tomcat/conf/server.xml
 
-STR5='certificateChainFile=\"conf\/localhost-rsa-chain.pem\"'
-sed -i 's/'"$STR5"'/'" "'/g' /opt/liferay/tomcat/conf/server.xml
+#STR3='type=\"RSA\"'
+#sed -i 's/'"$STR3"'/'" "'/g' /opt/liferay/tomcat/conf/server.xml
 
 cat /opt/liferay/tomcat/conf/server.xml
+
+
 
 
 
